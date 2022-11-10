750fa3551a6306d66bee9680b71e8125ee72e48f
Gq <1611266263@qq.com>
2022-11-10 15:57:53 +0800

    整合Swagger2进行接口测试  http://localhost:8800/doc.html

--- service-system/src/main/java/com/gogoq/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/gogoq/system/controller/SysRoleController.java
@@ -2,6 +2,7 @@ package com.gogoq.system.controller;
 
 import com.gogoq.system.model.system.SysRole;
 import com.gogoq.system.service.SysRoleService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +20,7 @@ public class SysRoleController {
     private SysRoleService sysRoleService;
 
     //查询所有记录
+    @ApiOperation("查询所有记录接口")
     @GetMapping("findAll")
     public List<SysRole> findAllRole(){
         //调用service
@@ -27,6 +29,7 @@ public class SysRoleController {
     }
 
     //逻辑删除
+    @ApiOperation("逻辑删除接口")
     @DeleteMapping("remove/{id}")
     public boolean removeRole(@PathVariable Long id){
         boolean b = sysRoleService.removeById(id);
